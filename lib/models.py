3ff02da171a8356b776c058b560702a432b8ed89
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-08-21 00:20:45 -0400

    Adds relationships to the models

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy import Column, String, Integer, Float, DateTime, create_engine, ForeignKey
 from datetime import datetime
 
@@ -11,11 +11,16 @@ class Expense(Base):
     amount = Column(Float(), nullable = False)
     description = Column(String())
     category_id = Column(Integer(), ForeignKey('categories.id'))
-    # timestamp = Column(DateTime, default = datetime.now())
+    time = Column(DateTime, default = datetime.now())
+    category = relationship("Category", back_populates="expenses")
+    
+    def __repr__(self):
+        return f"Amount: {amount}, description: {description}, category id: {category_id}"
 
 class Category(Base):
     __tablename__ = 'categories'
     id = Column(Integer(), primary_key = True)
     name = Column(String())
+    expense = relationship("Expense", back_populates="category")
     
 Base.metadata.create_all(engine)
