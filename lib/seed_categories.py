2294587a8852ac12538fb914a84c2efd1e20cde5
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-08-22 21:10:28 -0400

    Defines a list of common categories

--- lib/seed_categories.py
+++ lib/seed_categories.py
@@ -1,4 +1,26 @@
+from sqlalchemy.orm import sessionmaker
+from models import engine
+from models import Category
 
+Session = sessionmaker(bind=engine)
+
+expense_categories = [
+    "Housing",
+    "Transportation",
+    "Food",
+    "Entertainment",
+    "Healthcare",
+    "Debt Payments",
+    "Savings",
+    "Utilities",
+    "Personal Care",
+    "Clothing",
+    "Gifts and Donations",
+    "Education",
+    "Travel",
+    "Childcare",
+    "Miscellaneous"
+]
 
 
 
