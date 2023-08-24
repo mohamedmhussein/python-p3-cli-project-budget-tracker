777cb0f597894a29e762cdcff768e3752c3e9263
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-08-23 22:58:12 -0400

    Finalizes the seed_expenses.py file

--- lib/seed_expenses.py
+++ lib/seed_expenses.py
@@ -1,8 +1,7 @@
 from models import engine, Expense, Session
-from functions import delete_all_records
-from faker import faker
-
-faker = Faker()
+from functions import delete_all_records, create_expense
+import random
+from datetime import datetime, timedelta
 
 #Delete all existing expense records
 delete_all_records(Expense)
@@ -31,8 +30,17 @@ sample_expenses = [
     "Haircut and Styling"
 ]
 
-# for category in common_categories:
-#     create_category(category)
+for description in sample_expenses:
+    amount = round(random.uniform(1, 100),2) # random amount
+    category_id = random.randint(1,20)
+
+    start_date = datetime(2020, 1, 1)
+    end_date = datetime(2022, 12, 31)
+    random_num_days = random.randint(0, (end_date - start_date).days)
+    date = start_date + timedelta(days=random_num_days)
+    formatted_date = date.strftime("%Y-%m-%d")
+
+    create_expense(amount, description,category_id, formatted_date)
 
 
 
