ba8468c67f210dbca16c050db0789b307aff2b8e
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-08-25 14:20:02 -0400

    Installs the simple_term_menu library and includes it in the main code

--- lib/cli.py
+++ lib/cli.py
@@ -3,6 +3,7 @@ from models import Expense, Category
 import subprocess
 import sys
 import os
+from simple_term_menu import TerminalMenu
 
 def restart_script():
     script_path = os.path.abspath(__file__)  # Get the absolute path of the current script
@@ -20,41 +21,45 @@ if __name__ == "__main__":
     print("Manage your finances and track your expenses easily.")
     print("-----------------------------------")
 
-    print("Choose one of the following options (enter the number chosen):\n \n")
+    print("Choose one of the following options:\n \n")
 
-    print("1) Show all expenses")
-    print("2) Show categories")
-    print("3) Add an expense")
-    print("4) Add a new category")
-    print("5) Sort expenses")
-    print("6) Sum expenses")
-    user_choice = int(input(">>  "))
+    # print("1) Show all expenses")
+    # print("2) Show categories")
+    # print("3) Add an expense")
+    # print("4) Add a new category")
+    # print("5) Sort expenses")
+    # print("6) Sum expenses")
+    # user_choice = int(input(">>  "))
+    options = ["Show all expenses","Show categories","Add an expense","Add a new category","Sort expenses", "Sum expenses"]
+    terminal_menu = TerminalMenu(options)
+    user_choice = terminal_menu.show()
+    print(options[user_choice])
 
-    if user_choice == 1:
+    if user_choice == 0:
         display_data(Expense)
-        restart_script()
-    elif user_choice == 2:
+        # restart_script()
+    elif user_choice == 1:
         display_data(Category)
-        restart_script()
-    elif user_choice == 3:
+        # restart_script()
+    elif user_choice == 2:
         date = input("Enter the expense date (YYYY-MM-DD) ")
         category_id = input("Enter the expense category id ")
         amount = input("Enter the expense amount ")
         description = input("Enter the expense description ")
         create_expense(amount, description, category_id, date)
-        restart_script()
-    elif user_choice == 4:
+        # restart_script()
+    elif user_choice == 3:
         name = input("Enter the category name: ")
         create_category(name)
-        restart_script()
-    elif user_choice == 5:
+        # restart_script()
+    elif user_choice == 4:
         sort_choice = int(input("1) Ascending\n2) Descending \n>>"))
         if sort_choice == 1:
             sort_by_amount("ascending")
         elif sort_choice == 2:
             sort_by_amount("descending")
-        restart_script()
-    elif user_choice == 6:
+        # restart_script()
+    elif user_choice == 5:
         sum_expenses()
 
 
